f1d8251fdbe69b8c71f74fbd731c38639ec9e351
Harald Alvestrand <hta@chromium.org>
2014-08-21 14:12:02 +0200

    Some more review comments

--- lib/x264.py
+++ lib/x264.py
@@ -16,41 +16,41 @@ class X264Codec(encoder.Codec):
       ]
 
   def StartEncoder(self):
-    return encoder.Encoder(self, "")
+    return encoder.Encoder(self, '')
 
 
   def Execute(self, parameters, bitrate, videofile, workdir):
-    commandline = ("%(x264)s "
-      "--vbv-maxrate %(bitrate)d --vbv-bufsize %(bitrate)d --vbv-init 0.8 "
-      "--bitrate %(bitrate)d --fps %(framerate)d "
-      "--threads 1 "
-      "--rc-lookahead 0 "
-      "--ref 2 "
-      "--profile baseline --no-scenecut --keyint infinite --preset veryslow "
-      "--input-res %(width)dx%(height)d "
-      "--tune psnr "
-      "-o %(outputfile)s %(inputfile)s") % {
-     'x264': encoder.Tool('x264'),
-     'bitrate': bitrate,
-     'framerate': videofile.framerate,
-     'width': videofile.width,
-     'height': videofile.height,
+    commandline = ('%(x264)s '
+      '--vbv-maxrate %(bitrate)d --vbv-bufsize %(bitrate)d --vbv-init 0.8 '
+      '--bitrate %(bitrate)d --fps %(framerate)d '
+      '--threads 1 '
+      '--rc-lookahead 0 '
+      '--ref 2 '
+      '--profile baseline --no-scenecut --keyint infinite --preset veryslow '
+      '--input-res %(width)dx%(height)d '
+      '--tune psnr '
+      '-o %(outputfile)s %(inputfile)s') % {
+      'x264': encoder.Tool('x264'),
+      'bitrate': bitrate,
+      'framerate': videofile.framerate,
+      'width': videofile.width,
+      'height': videofile.height,
       'outputfile': workdir + '/' + videofile.basename + self.extension,
       'inputfile': videofile.filename}
     print commandline
     with open('/dev/null', 'r') as nullinput:
       returncode = subprocess.call(commandline, shell=True, stdin=nullinput)
       if returncode:
-        raise Exception("Encode failed with returncode %d" % returncode)
+        raise Exception('Encode failed with returncode %d' % returncode)
     return self.Measure(bitrate, videofile, workdir)
 
   def Measure(self, bitrate, videofile, workdir):
     result = {}
-    tempyuvfile = "%s/%stempyuvfile.yuv" % (workdir, videofile.basename)
+    tempyuvfile = '%s/%stempyuvfile.yuv' % (workdir, videofile.basename)
     if os.path.isfile(tempyuvfile):
-      print "Removing tempfile before decode:", tempyuvfile
+      print 'Removing tempfile before decode:', tempyuvfile
       os.unlink(tempyuvfile)
-    commandline = "%s -i %s/%s.%s %s" % (
+    commandline = '%s -i %s/%s.%s %s' % (
       encoder.Tool('ffmpeg'),
       workdir, videofile.basename, self.extension, tempyuvfile)
     print commandline
@@ -61,13 +61,13 @@ class X264Codec(encoder.Codec):
     bitrate = videofile.MeasuredBitrate(
       os.path.getsize('%s/%s.%s' % (workdir, videofile.basename,
                                     self.extension)))
-    commandline = "%s %s %s %d %d 9999" % (
+    commandline = '%s %s %s %d %d 9999' % (
       encoder.Tool('psnr'),
       videofile.filename, tempyuvfile, videofile.width,
       videofile.height)
     print commandline
     psnr = subprocess.check_output(commandline, shell=True)
-    print "Bitrate", bitrate, "PSNR", psnr
+    print 'Bitrate', bitrate, 'PSNR', psnr
     result['bitrate'] = int(bitrate)
     result['psnr'] = float(psnr)
     os.unlink(tempyuvfile)
