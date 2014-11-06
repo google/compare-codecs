11137e7c242e8f031b7e54398a851166071e9f93
Harald Alvestrand <hta@chromium.org>
2014-11-05 16:49:52 -0800

    Improved styling of results tables.
    This CL uses Javascript to attach styles to table cells, and uses
    CSS to style these cells, resulting in  a reasonably readable look.

--- website/assets/js/codecfuncs.js
+++ website/assets/js/codecfuncs.js
@@ -9,8 +9,26 @@ function FillInAllTables() {
     dataType: 'json',
     async: false
   }).responseText;
+  var tableStyles = { allowHtml: true,
+                      alternatingRowStyle: false,
+                      showRowNumber: false,
+                      sort: 'disable',
+                      // Width is problematic, cross-device.
+                      width: '50%',
+                      cssClassNames: {
+                        // These map to .rslts-* classes
+                        headerRow: 'rslts-headerRow',
+                        tableRow: 'rslts-tableRow',
+                        oddTableRow: 'rslts-oddTableRow',
+                        selectedTableRow: 'rslts-selectedTableRow',
+                        hoverTableRow: 'rslts-hoverTableRow',
+                        headerCell: 'rslts-headerCell',
+                        tableCell: 'rslts-tableCell',
+                        rowNumberCell: 'rslts-rowNumberCell',
+                      }
+                    };
   var data = new google.visualization.DataTable(tablesAsJson);
   var table = new google.visualization.Table(
     document.getElementById('basic-results'));
-  table.draw(data, {allowHtml: true});
+  table.draw(data, tableStyles);
 }
